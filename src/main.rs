dab2f72c6ef3c73d0bb54387ddaa46c088bc82fe
sun <minhua.sun@aixplorerbio.com>
2023-02-24 16:32:39 +0800

    support cors

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@
 use std::net::Ipv4Addr;
 
 use response::response_ok;
-use rocket::{fs::NamedFile, get, info, launch, log::LogLevel, response::content, routes};
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    fs::NamedFile,
+    get,
+    http::Header,
+    info, launch,
+    log::LogLevel,
+    response::content,
+    routes, Request, Response,
+};
 use utils::{file_exist, get_pmid_path_by_id};
 
 use crate::response::response_error;
@@ -92,11 +101,34 @@ async fn rocket() -> _ {
     cfg.port = 4321;
 
     rocket::custom(cfg)
+        .attach(Cors) // cors
         .mount(
             "/api",
             routes![query_pubmed, get_pubmed_by_id, query_pubmed_total],
         )
         .mount("/", routes![get_pubmed])
+}
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
 
 #[cfg(test)]

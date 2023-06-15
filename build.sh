7deeb1651cf0594c9f3d295a73c539d95eee7eee
sun <minhua.sun@aixplorerbio.com>
2023-06-15 09:20:47 +0800

    更新build.sh文件，通过git pull获取最新的代码并进行构建。同时重新构建docker镜像并重启docker-compose。

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm -rf web/dist
 echo "0. start build front..."
-cd ../vue-eutils && npm run build
-cd - 
+cd ../vue-eutils && git pull origin master && npm run build
+cd -
 echo "1. start build --release..."
 cargo build --release
 echo "2. rebuild docker image..."
 sudo docker build -t asmh1989/rust-eutils .
 echo "3. restart docker-compose..."
-sudo docker-compose up -d 
+sudo docker-compose up -d
 echo "done..."
